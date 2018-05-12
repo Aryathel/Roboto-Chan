e267d73d8d91b14026b3e822c88475b3699c2a9a
HeroicosHM <houghtonawe@gmail.com>
2018-05-12 18:23:54 +0000

    Refined user entry, fixed some movement bugs, started implementing Cartesian movements

--- RobotGUI.py
+++ RobotGUI.py
@@ -1,115 +1,205 @@
 #Import Tkinter Module
-from Tkinter import *
+import Tkinter as tk
 import serial
 import time
 
-time.sleep(3)
 
-#Set up the USB readin for the Serial cable to the Arduino
-ser = serial.Serial('/dev/ttyUSB0', 9600) #needs to be updated with proper terminal later
+#Set up the USB reading for the Serial cable to the Arduino
+ser = serial.Serial('/dev/ttyUSB1', 9600) #needs to be updated with proper terminal later
+print ser.name
+
+time.sleep(5)
 
 #Start the Root GUI
-root = Tk()
+root = tk.Tk()
 
 #Reading the Serial Monitor to find the current values
+actuate = ser.write('s')
+time.sleep(0.5)
+data = ser.readline()
 
-
-#Function for Updating Data
-def updateData():
-	global data
-	data = ser.readline().split("  ")
-	print(data)
-
-updateData()
+print data
+entries = data.split(" ")
 
 #Parameters for the Root Window
 root.title("Roboto-chan Controls")
 root.geometry("800x800")
 
+#Function for Updating the Servo Positions
+def updateData():
+	global data
+	actuate = ser.write('s')
+	time.sleep(0.5)
+	data = ser.readline()
+	print(data)
+	entries = data.split(" ")
+	return entries
+
+#Function For Reading Location
+def updateLocs():
+	updateData()
+	time.sleep(0.5)
+	angleBase = entries[4]
+	print angleBase
+	currentBaseAngleLabel['text'] = "Base Servo: " + angleBase
+
+	angleRight = entries[5]
+	print angleRight
+	currentRightAngleLabel['text'] = "Right Servo: " + angleRight
+
+	angleLeft = entries[6]
+	print angleLeft
+	currentLeftAngleLabel['text'] = "Left Servo: " + angleLeft
+
 #Entries Function for User Text Entry Test
 def submit_entries():
-	X = entry1.get()
-	Y = entry2.get()
-	Z = entry3.get()
-	print("X: %s\nY: %s\nZ: %s" % (X, Y, Z))
+
+	#Read the Entry Fields
+	Base = entry1.get()
+	Right = entry2.get()
+	Left = entry3.get()
+
+	#Safety Functions To Keep The Servo Within Our Bounds
+	if int(Base) > 180:
+		print("Angle Can Not Be Greater Than 180 Degrees!")
+		Base = 180
+	if int(Base) < 64:
+		print("Angle Can Not Be Less Than 64 Degrees!")
+		Base = 64
+
+	if int(Right) > 180:
+		print("Angle Can Not Be Greater Than 180 Degrees!")
+		Right = 180
+	if int(Right) < 90:
+		print("Angle Can Not Be Less Than 90 Degrees!")
+		Right = 90
+
+	if int(Left) > 160:
+		print("Angle Can Not Be Greater Than 160 Degrees!")
+		Left = 160
+	if int(Left) < 51:
+		print("Angle Can Not Be Less Than 51 Degrees!")
+		Left = 51
+
+	print("Base: %s\nRight: %s\nLeft: %s" % (Base, Right, Left))
 
 #Test Function for Buttons
 def test():
 	print("This is a test button.")
 
-#Button Control for Simple Movement
+#Functions for Simple Movement
 def rotateRight():
-	updateData()
+	updateLocs()
 	increment = entry4.get()
-	angleBase = data[4].split(" ")
-	newAngleBase = int(angleBase[2]) + int(increment)
-	print(angleBase[2])
+	angleBase = entries[4]
+	newAngleBase = int(angleBase) + int(increment)
+	print(angleBase)
 	if newAngleBase > 180:
-		print("Angle Can Not Be Larger Than 180 Degrees!")
+		print("Angle Can Not Be Greater Than 180 Degrees!")
 		newAngleBase = 180
 	print(newAngleBase)
 
 def rotateLeft():
+	updateData()
 	increment = entry4.get()
-	angleBase = data[4].split(" ")
-	newAngleBase = int(angleBase[2]) - int(increment)
-	print(angleBase[2])
+	angleBase = entries[4]
+	newAngleBase = int(angleBase) - int(increment)
+	print(angleBase)
 	if newAngleBase < 64:
 		print("Angle Can Not Be Less Than 64 Degrees!")
 		newAngleBase = 64
 	print(newAngleBase)
 
 def rotateRightServoForward():
+	updateData()
 	increment = entry4.get()
-	angleRight = data[5].split(" ")
-	newAngleRight = int(angleRight[2]) + int(increment)
-	print(angleRight[2])
+	angleRight = entries[5]
+	newAngleRight = int(angleRight) + int(increment)
+	print(angleRight)
 	if newAngleRight > 180:
 		print("Angle Can Not Be Greater Than 180 Degrees!")
 		newAngleRight = 180
-	print(newAngleBase)
+	print(newAngleRight)
 
 def rotateRightServoBackward():
+	updateData()
 	increment = entry4.get()
-	angleRight = date[5].split(" ")
-	newAngleRight = int(angleRight[2]) - int(increment)
-	print(angleRight[2])
+	angleRight = entries[5]
+	newAngleRight = int(angleRight) - int(increment)
+	print(angleRight)
 	if newAngleRight < 90:
 		print("Angle Can Not Be Less Than 90 Degrees!")
 		newAngleRight = 90
 	print(newAngleRight)
 
-#User Text Entry Test
-Label(root, text = "X").grid(row = 0)
-Label(root, text = "Y").grid(row = 1)
-Label(root, text = "Z").grid(row = 2)
+def rotateLeftServoForward():
+	updateData()
+	increment = entry4.get()
+	angleLeft = entries[6]
+	newAngleLeft = int(angleLeft) + int(increment)
+	print(angleLeft)
+	if newAngleLeft > 160:
+		print("Angle Can Not Be Greater Than 160 Degrees!")
+		newAngleLeft = 160
+	print(newAngleLeft)
 
-entry1 = Entry(root)
-entry2 = Entry(root)
-entry3 = Entry(root)
+def rotateLeftServoBackward():
+	updateData()
+	increment = entry4.get()
+	angleLeft = entries[6]
+	newAngleLeft = int(angleLeft) - int(increment)
+	print(angleLeft)
+	if newAngleLeft < 51:
+		print("Angle Can Not Be Less Than 51 Degrees!")
+		newAngleLeft = 51
+	print(newAngleLeft)
 
-entry1.grid(row = 0, column = 1)
-entry2.grid(row = 1, column = 1)
-entry3.grid(row = 2, column = 1)
+#Labels For Reading The Current Locations of the Servos
+currentBaseAngleLabel = tk.Label(root)
+currentBaseAngleLabel.grid(row = 0, column = 0)
+
+currentRightAngleLabel = tk.Label(root)
+currentRightAngleLabel.grid(row = 1, column = 0)
+
+currentLeftAngleLabel = tk.Label(root)
+currentLeftAngleLabel.grid(row = 2, column = 0)
+
+#User Text Entry For Simple Movements
+tk.Label(root, text = "X").grid(row = 0, column = 2)
+tk.Label(root, text = "Y").grid(row = 1, column = 2)
+tk.Label(root, text = "Z").grid(row = 2, column = 2)
+
+entry1 = tk.Entry(root)
+entry2 = tk.Entry(root)
+entry3 = tk.Entry(root)
+
+entry1.grid(row = 0, column = 3)
+entry2.grid(row = 1, column = 3)
+entry3.grid(row = 2, column = 3)
 
 #Buttons For Entry Fields
-Button(root, text = "Quit", command = root.quit).grid(row = 3, column = 0, pady = 10)
-Button(root, text = "Submit", command = submit_entries).grid(row = 3, column = 2, pady = 10)
+tk.Button(root, text = "Quit", command = root.quit).grid(row = 3, column = 0, pady = 10)
+tk.Button(root, text = "Submit", command = submit_entries).grid(row = 3, column = 2, pady = 10)
 
-Label(root, text = "Movement Increment:").grid(row = 4, pady = 10)
 
-entry4 = Entry(root)
+#Simple Movement Increment Field
+tk.Label(root, text = "Movement Increment:").grid(row = 4, pady = 10)
+
+entry4 = tk.Entry(root)
 entry4.grid(row = 5)
 entry4.insert(5, "10")
 
+#Buttons For Simple Movements
+tk.Button(root, text = "Left Servo Forward", command = rotateLeftServoForward).grid(row = 8, column = 3)
+tk.Button(root, text = "Left Servo Backward", command = rotateLeftServoBackward).grid(row = 10, column = 3)
 
-Button(root, text = "Move Forward", command = test).grid(row = 8, column = 3)
-Button(root, text = "Rotate Left", command = rotateLeft).grid(row = 9, column = 2)
-Button(root, text = "Activate/Release Claw", command = test).grid(row = 9, column = 3)
-Button(root, text = "Rotate Right", command = rotateRight).grid(row = 9, column = 4)
-Button(root, text = "Move Backward", command = test).grid(row = 10, column = 3) 
-Button(root, text = "Move Upwards", command = test).grid(row = 8, column = 1)
-Button(root, text = "Move Downwards", command = test).grid(row = 10, column = 1)
+tk.Button(root, text = "Rotate Base Left", command = rotateLeft).grid(row = 8, column = 5)
+tk.Button(root, text = "Rotate Base Right", command = rotateRight).grid(row = 10, column = 5)
+
+tk.Button(root, text = "Right Servo Forward", command = rotateRightServoForward).grid(row = 8, column = 1)
+tk.Button(root, text = "Right Servo Backward", command = rotateRightServoBackward).grid(row = 10, column = 1)
+
+tk.Button(root, text = "Activate/Release Claw", command = test).grid(row = 9, column = 0)
 
 #Restart the Loop
 root.mainloop()
--- ServoControl/ServoControl.ino
+++ ServoControl/ServoControl.ino
@@ -1,6 +1,11 @@
 //Including Servo Library
 #include <Servo.h>
 
+const byte numChars = 32;
+char receivedChars[numChars];
+
+boolean newData = false;
+
 //Defining Servos
 Servo base;
 Servo right;
@@ -8,22 +13,47 @@ Servo left;
 Servo claw;
 
 //Servo Position Defaults, in Degrees
-int baseStart = 131;
-int rightStart = 120;
-int leftStart = 120;
+int baseStart = 111; 
+int rightStart = 87; 
+int leftStart = 167; 
 int clawStart = 48;
 
 //Define Analog Input Servo Pins
 
 const char basePin = A1;
-const char leftPin = A2;
-const char rightPin = A3;
+const char rightPin = A2;
+const char leftPin = A3;
 const char clawPin = A4;
 
+//Defining String variable to take commands
+String string;
+
+int baseVoltMid;
+int rightVoltMid;
+int leftVoltMid;
+int clawVoltMid;
+
+int baseVoltMax;
+int rightVoltMax;
+int leftVoltMax;
+int clawVoltMax;
+
+int baseVoltMin;
+int rightVoltMin;
+int leftVoltMin;
+int clawVoltMin;
+
+int baseMin = 64;
+int baseMax = 180;
+
+int rightMin = 46;
+int rightMax = 156;
+
+int leftMin = 100;
+int leftMax = 180;
+
+
 void setup() {
-  //Start Serial Monitor
-  Serial.begin(9600);
-  
   //Define Pin States
   pinMode(basePin, INPUT);
   pinMode(leftPin, INPUT);
@@ -36,14 +66,203 @@ void setup() {
   left.attach(6);
   claw.attach(9);
   
+  base.write(180);
+  right.write(156);
+  left.write(175);
+  claw.write(123);
+  
+  delay(1500);
+  
+  baseVoltMax = analogRead(basePin);
+  rightVoltMax = analogRead(rightPin);
+  clawVoltMax = analogRead(clawPin);
+  
+  base.write(64);
+  right.write(46);
+  left.write(135);
+  claw.write(123);
+  
+  delay(1500);
+  
+  baseVoltMin = analogRead(basePin);
+  rightVoltMin = analogRead(rightPin);
+  clawVoltMin = analogRead(clawPin);
+  
   //Move Servos To Starting Locations
   base.write(baseStart);
   right.write(rightStart);
   left.write(leftStart);
   claw.write(clawStart);
+  
+  delay(1500);
+  
+  baseVoltMid = analogRead(basePin);
+  rightVoltMid = analogRead(rightPin);
+  leftVoltMid = analogRead(leftPin);
+  clawVoltMid = analogRead(clawPin);
+  
+  left.write(180);
+  
+  delay(1500);
+  
+  leftVoltMax = analogRead(leftPin);
+  
+  left.write(100);
+  
+  delay(1500);
+  
+  leftVoltMin = analogRead(leftPin);
+  
+  left.write(167);
+  
+  delay(500);
+  
+  //Start Serial Monitor
+  Serial.begin(9600);
 }
 
 void loop() {
+  while (Serial.available()==0) {}
+
+  byte instr = Serial.read();
+
+  if(instr == 'd'){
+    while(Serial.available() == 0) {}
+    
+    byte whichServos = Serial.read();
+    
+    if((whichServos & 'a') == 'a') {
+      getBaseAngle();
+    }
+
+    if((whichServos & 'b') == 'b') {
+      getRightAngle();
+    }
+
+    if((whichServos & 'd') == 'd') {
+      getLeftAngle();
+    }
+
+    if((whichServos & 'h') == 'h') {
+      getClawAngle();
+    }
+  }
+
+  if(instr == 'a'){
+    getDataPrint();
+  }
+  
+  if(instr == 's') {
+    boolean Loop = true;
+    while(Serial.available() == 0) {}
+    
+    char whichServo = Serial.read();
+    
+    if(whichServo == '1') {
+      while(Loop) {
+        readNewBase();
+        if(newData == true) {
+          int angle = atoi(receivedChars);
+          newData = false;
+          Loop = false;
+          base.write(angle);
+        }
+      }
+    }
+    
+    if(whichServo == '2') {
+      while(Loop) {
+        readNewBase();
+        if(newData == true) {
+          int angle = atoi(receivedChars);
+          newData = false;
+          Loop = false;
+          right.write(angle);
+        }
+      }
+    }
+    
+    if(whichServo == '3') {
+      while(Loop) {
+        readNewBase();
+        if(newData == true) {
+          int angle = atoi(receivedChars);
+          newData = false;
+          Loop = false;
+          left.write(angle);
+        }
+      }
+    }
+  }
+}
+
+void readNewBase() {
+  static byte ndx = 0;
+  char endMarker = ',';
+  char data;
+
+  while(Serial.available() > 0 && newData == false) {
+    data = Serial.read();
+    
+    if(data != endMarker) {
+      receivedChars[ndx] = data;
+      ndx++;
+      if(ndx >= numChars) {
+        ndx = numChars-1;
+      }
+    } else {
+      receivedChars[ndx] = '\0';
+      ndx = 0;
+      newData = true;
+    }
+  }
+}
+
+void readNewRight() {
+  static byte ndx = 0;
+  char endMarker = ',';
+  char data;
+
+  while(Serial.available() > 0 && newData == false) {
+    data = Serial.read();
+    
+    if(data != endMarker) {
+      receivedChars[ndx] = data;
+      ndx++;
+      if(ndx >= numChars) {
+        ndx = numChars-1;
+      }
+    } else {
+      receivedChars[ndx] = '\0';
+      ndx = 0;
+      newData = true;
+    }
+  }
+}
+
+void readNewLeft() {
+  static byte ndx = 0;
+  char endMarker = ',';
+  char data;
+
+  while(Serial.available() > 0 && newData == false) {
+    data = Serial.read();
+    
+    if(data != endMarker) {
+      receivedChars[ndx] = data;
+      ndx++;
+      if(ndx >= numChars) {
+        ndx = numChars-1;
+      }
+    } else {
+      receivedChars[ndx] = '\0';
+      ndx = 0;
+      newData = true;
+    }
+  }
+}
+
+void getDataPrint() {
   //Define Variables for Reading Servo Positions
   int basePos;
   int rightPos;
@@ -52,7 +271,7 @@ void loop() {
   int baseAngle;
   int rightAngle;
   int leftAngle;
-  int clawAngle;
+  String clawAngle = "???";
   
   //Read the Current Position of the Servos in Voltage
   basePos = analogRead(basePin);
@@ -61,28 +280,77 @@ void loop() {
   clawPos = analogRead(clawPin);
 
   //Print the Servo Angles, in Voltage, to the Serial Monitor
-  Serial.print("BVP: ");
   Serial.print(basePos);
-  Serial.print("   RVP: ");
+  Serial.print(" ");
   Serial.print(rightPos);
-  Serial.print("   LVP: ");
+  Serial.print(" ");
   Serial.print(leftPos);
-  Serial.print("   CVP: ");
+  Serial.print(" ");
   Serial.print(clawPos);
 
   //Read the Current Position of the Servos in Voltage
-  baseAngle = map(basePos, 247, 494, 64, 180);
-  rightAngle = map(rightPos, 297, 485, 90, 180);
-  leftAngle = map(leftPos, 213, 440, 51, 160);
-  clawAngle = "???";
+  baseAngle = map(basePos, baseVoltMin, baseVoltMax, baseMin, baseMax);
+  rightAngle = map(rightPos, rightVoltMin, rightVoltMax, rightMin, rightMax);
+  
+  leftAngle = map(leftPos, leftVoltMin, leftVoltMax, leftMin, leftMax);
 
   //Print the Servo Angles, in Degrees, to the Serial Monitor
-  Serial.print("   BAA: ");
+  Serial.print(" ");
   Serial.print(baseAngle);
-  Serial.print("   RAA: ");
+  Serial.print(" ");
   Serial.print(rightAngle);
-  Serial.print("   LAA: ");
+  Serial.print(" ");
   Serial.print(leftAngle);
-  Serial.print("   CAA: ");
+  Serial.print(" ");
   Serial.println(clawAngle);
+}
+
+void getBaseAngle() {
+  //Define Variables for Reading Servo Positions
+  int basePos;
+  int baseAngle;
+  
+  //Read the Current Position of the Servos in Voltage
+  basePos = analogRead(basePin);
+
+  //Read the Current Position of the Servos in Voltage
+  baseAngle = map(basePos, baseVoltMin, baseVoltMax, baseMin, baseMax);
+
+  Serial.println(baseAngle);
+}
+
+void getRightAngle() {
+  //Define Variables
+  int rightPos;
+  int rightAngle;
+
+  rightPos = analogRead(rightPin);
+  
+  //Read the Current Position of the Servos in Voltage
+  rightAngle = map(rightPos, rightVoltMin, rightVoltMax, rightMin, rightMax);
+    
+  Serial.println(rightAngle);
+}
+
+void getLeftAngle() {
+  //Define Variables for Reading Servo Positions
+  int leftPos;
+  int leftAngle;
+  
+  //Read the Current Position of the Servos in Voltage
+  leftPos = analogRead(leftPin);
+
+  //Read the Current Position of the Servos in Voltage
+  leftAngle = map(leftPos, leftVoltMin, leftVoltMax, leftMin, leftMax);
+  leftAngle = map(leftPos, 247, 494, 64, 180);
+
+  Serial.println(leftAngle);
+}
+
+void getClawAngle() {
+  int clawPos;
+
+  clawPos = analogRead(clawPin);
+  
+  Serial.println("???");
 }
